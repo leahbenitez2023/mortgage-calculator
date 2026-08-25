74d2249b6da9fbb73c6781c4f17e6c7a7dc7b6f3
leahbenitez2023 <leah.benitez2023@gmail.com>
2026-08-24 21:44:02 -0400

    Rename main app

--- src/main/java/com/example/mortgage/MortgageCalculator.java
+++ src/main/java/com/example/mortgage/MortgageCalculator.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.mortgage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MortgageCalculator {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MortgageCalculator.class, args);
 	}
 
 }
